2917506beaef0f7e23682e8d4f63de6ad6dd8144
diully <diully_almeida@estudante.sesisenai.org.br>
2026-08-18 16:29:34 -0300

    exercicio 2 feito

--- exercicios.js
+++ exercicios.js
@@ -34,8 +34,10 @@ console.log("EX 1 - Divisão:", num1 / num2);
 // Converta para Fahrenheit e mostre no console.
 // Fórmula: F = C * 9/5 + 32
 // ------------------------------------------------------------
+let celsius = 25;
+let fahrenheit = celsius * 9 / 5 + 32;
 
-
+console.log(fahrenheit);
 
 
 // EXERCÍCIO 3 - Par ou ímpar
