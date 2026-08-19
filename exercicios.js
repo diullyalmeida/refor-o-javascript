a90beb74f51852775dbddd3068302145b20abc53
diully <diully_almeida@estudante.sesisenai.org.br>
2026-08-18 22:04:26 -0300

    exercicio 21 feito

--- exercicios.js
+++ exercicios.js
@@ -443,7 +443,17 @@ console.log(pares);
 // Ao clicar em cada botão, mudar a cor de fundo da página.
 // Dica: document.body.style.backgroundColor = 'red'
 // ------------------------------------------------------------
+function vermelho() {
+    document.body.style.backgroundColor = "red";
+}
 
+function azul() {
+    document.body.style.backgroundColor = "blue";
+}
+
+function verde() {
+    document.body.style.backgroundColor = "green";
+}
 
 
 
